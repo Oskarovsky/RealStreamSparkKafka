c662c9e7610edddc96a13a48b3d15e7d1b1c358e
Oskarovsky <oskar.slyk@gmail.com>
2021-05-15 15:00:42 +0200

    stream

--- src/main/scala/com/oskarro/config/Constants.scala
+++ src/main/scala/com/oskarro/config/Constants.scala
@@ -1,5 +1,25 @@
 package com.oskarro.config
 
+import java.time.format.DateTimeFormatter
+import java.util.Properties
+
 object Constants {
+
+  var appName: String = "DStreamSparkApp"
+  var masterValue: String = "local[*]"
+
+  var busTopic01 = "temat_bus01"
+
+  val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+
+  val properties: Properties = new Properties()
+  properties.put("bootstrap.servers", "localhost:9092")
+  properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserialization")
+  properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserialization")
+  properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+  var apiKey: String = "3b168711-aefd-4825-973a-4e1526c6ce93"
+  var resourceID: String = "2e5503e-927d-4ad3-9500-4ab9e55deb59"
 
 }
--- src/main/scala/com/oskarro/service/KafkaService.scala
+++ src/main/scala/com/oskarro/service/KafkaService.scala
@@ -1,5 +1,25 @@
 package com.oskarro.service
 
+/**
+ * KafkaService is a class that handle kafka process
+ */
+
 class KafkaService {
+
+  /**
+   * Send data to kafka topic with specific configurations
+   * run locally with 4 cores, or "spark://master:7077" to run on a Spark standalone cluster.
+   *
+   * @param info is a string with basic information for client,
+   * @param topic contains topic name,
+   * @param props defines configuration,
+   * @param content is sending on a topic
+   */
+  def writeToKafka(info: String, topic: String, props: Properties = Constants.properties, content: String): Unit = {
+    val producer = new KafkaProducer[String, String](props)
+    val record = new ProducerRecord[String, String](topic, content)
+    producer.send(record)
+    producer.close()
+  }
 
 }
